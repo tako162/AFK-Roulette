ca2dce9b0c6f659e9c16ffe4dedb6b9bfdcc3202
tako162 <154701396+tako162@users.noreply.github.com>
2025-05-17 13:17:21 +0000

    fix

--- startup/interaction_create_handler.js
+++ startup/interaction_create_handler.js
@@ -5,23 +5,12 @@ const Discord = require("discord.js");
 exports.call = async function (client) {
   client.on("interactionCreate", async (interaction) => {
     // コマンド実行時
-    try {
-      if (interaction.type === Discord.InteractionType.ApplicationCommand) {
-        // 指定されたコマンドが見つからなければ中断
-        const command = client.commands.get(interaction.commandName);
-        if (!command) return;
-        // コマンドを実行する
-        const role = interaction.guild.roles.cache.find(
-          (roles) => roles.name === "GameMaster",
-        );
-        if (!interaction.member.roles.cache.has(role.id)) {
-          return interaction.reply("権限がありません。");
-        } else {
-          await command.execute(interaction, client);
-        }
-      }
-    } catch (error) {
-      client.channels.cache.get(config.channels.log).send(error);
+    if (interaction.type === Discord.InteractionType.ApplicationCommand) {
+      // 指定されたコマンドが見つからなければ中断
+      const command = client.commands.get(interaction.commandName);
+      if (!command) return;
+
+      await command.execute(interaction, client);
     }
   });
 };
